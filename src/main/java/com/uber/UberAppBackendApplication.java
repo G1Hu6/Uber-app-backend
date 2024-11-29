b89376fad28ac82ffe4fadb2f4db996401df5c26
G1Hu6 <r1ruru219@gmail.com>
2024-11-29 15:34:17 +0530

    creating uber-project and adding dependencies

---
+++ src/main/java/com/uber/UberAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAppBackendApplication.class, args);
+	}
+
+}
