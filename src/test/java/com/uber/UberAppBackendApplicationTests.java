61abe4a767c3ec5d9085cdd812b73a95b799f6ee
G1Hu6 <r1ruru219@gmail.com>
2025-01-10 17:30:32 +0530

    Adding email sending service

--- src/test/java/com/uber/UberAppBackendApplicationTests.java
+++ src/test/java/com/uber/UberAppBackendApplicationTests.java
@@ -1,13 +1,33 @@
 package com.uber;
 
+import com.uber.services.EmailSenderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberAppBackendApplicationTests {
 
+	@Autowired
+	private EmailSenderService emailSenderService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	// Testing mail service
+	@Test
+	void testingEmailService(){
+		emailSenderService.sendEmail("rushikarle45@gmail.com","This is Reminder for come tomarow early","Hi Rushikesk how are you....");
+	}
+
+	@Test
+	void testingMultipleEmails(){
+		String[] emails = {
+				"harshalwadne1978@gmail.com",
+				"rushikarle45@gmail.com",
+		};
+		emailSenderService.sendEmail(emails,"Hello Harshal Wande","hello harshal I am sending email from spring-boot application please read this email and call back me please");
 	}
 
 }
